4bbd3d493db2380447b33d8f7c210837dabbd2ae
Abhinav kumar <abhinavkrhzb@gmail.com>
2026-06-27 16:42:16 +0530

    uploaded all files

---
+++ backend_with_cors.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:8080"
+    ],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "Hello from Backend with CORS"
+    }
