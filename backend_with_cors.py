914db14755062cb5cc996a7c9b81071959045fb2
Abhinav kumar <abhinavkrhzb@gmail.com>
2026-06-27 19:29:30 +0530

    Update CORS configuration and add screenshots

--- backend_with_cors.py
+++ backend_with_cors.py
@@ -8,7 +8,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:8080"
+        "http://127.0.0.1:8080",
+        "http://127.0.0.1:5500"
     ],
     allow_methods=["*"],
     allow_headers=["*"],
